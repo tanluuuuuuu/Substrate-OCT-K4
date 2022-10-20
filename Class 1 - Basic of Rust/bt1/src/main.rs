da2b4c07678947ea86c72bffe8fef79f45e8e142
tanluuuuuuu <20521591@gm.uit.edu.vn>
2022-10-20 12:28:02 +0700

    correct

--- Class 1 - Basic of Rust/bt1/src/main.rs
+++ Class 1 - Basic of Rust/bt1/src/main.rs
@@ -3,26 +3,52 @@
 //         let sub_arr = [6,8,10];
 #![allow(unused)]
 
+use core::num;
+
 fn main() {
-    let org_arr: [i32; 8] = [1,2,3,5,6,8,10,11];
-    let sub_arr: [i32; 3] = [6,8,10];
+    let org_arr = vec![1,6,3,5,6,8,10,11];
+    let sub_arr = vec![6,8,10];
 
     // Brute-force
     // Check if every number in sub array appear in original array
-    for num_in_sub_arr in sub_arr {
-        let mut flag = false;
-        for num_in_org_arr in org_arr {
-            if (num_in_sub_arr == num_in_org_arr)
-            {
-                flag = true;
+    // for num_in_sub_arr in sub_arr {
+    //     let mut flag = false;
+    //     for num_in_org_arr in org_arr {
+    //         if (num_in_sub_arr == num_in_org_arr)
+    //         {
+    //             flag = true;
+    //             break;
+    //         }
+    //     }
+    //     if (!flag)
+    //     {
+    //         println!("Not subarray");
+    //         break;
+    //     }
+    // }
+    // println!("Subarray");
+    
+    let mut it = sub_arr.iter();
+    let mut number = it.next();
+    for (idx, num_in_org_arr) in org_arr.iter().enumerate() {
+        match number {
+            Some(x) => {
+                if (x == num_in_org_arr)
+                {
+                    number = it.next();
+                }
+            },
+            None => {
+                println!("Subarray");
                 break;
             }
         }
-        if (!flag)
-        {
-            println!("Not subarray");
-            break;
+    }
+    match number { 
+        Some(x) => {
+            println!("Not Subarray");
+        },
+        None => {
         }
     }
-    println!("Subarray");
 }
